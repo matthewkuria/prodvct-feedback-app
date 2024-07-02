da857f013be2298a5cbadb795f71a19adeef08e6
matthewkuria <mathewkwachira@gmail.com>
2024-07-02 23:07:35 +0300

    Create the sign up Form

--- my-project/app/ui/login/sign-up-form.tsx
+++ my-project/app/ui/login/sign-up-form.tsx
@@ -1,27 +1,27 @@
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+
 import { z } from "zod"
 
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
+
+import { Input } from "@/app/components/ui/input"
+import { toast } from "@/app/components/ui/use-toast"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../../components/ui/form"
+import { Button } from "../../components/ui/button"
+import { useForm } from "react-hook-form"
 
 
 const FormSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
+   email: z
+    .string()
+    .min(1, { message: "This field has to be filled." })
+    .email("This is not a valid email.")
+    .refine((e) => e === "abcd@fg.com", "This email is not in our database"),
+   password: z.string().min(4),
 })
 
 export function SignUpForm() {
@@ -44,26 +44,49 @@ export function SignUpForm() {
   }
 
   return (
-    <Form {...form}>
+    <main className="flex-col justify-center items-center mx-auto max-w-md">
+      <h1 className="text-3xl">Sign Up</h1>
+      <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
         <FormField
           control={form.control}
           name="username"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
+            <FormItem>             
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+                <Input placeholder="Username" {...field} />
+              </FormControl>             
+              <FormMessage />
+            </FormItem>
+          )}
+          />
+          <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>              
+              <FormMessage />
+            </FormItem>
+          )}
+          />
+          <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Password" {...field} />
+              </FormControl>              
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button className="" type="submit">Submit</Button>
       </form>
     </Form>
+    </main>
   )
 }
