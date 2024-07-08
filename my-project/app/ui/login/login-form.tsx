70a6507566fc8112eea8b15dbb9635aeb8e7c5d1
matthewkuria <mathewkwachira@gmail.com>
2024-07-08 23:28:16 +0300

    Update: the configuration files

--- my-project/app/ui/login/login-form.tsx
+++ my-project/app/ui/login/login-form.tsx
@@ -3,60 +3,95 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 import { Input } from "@/app/components/ui/input"
-import { toast } from "@/app/components/ui/use-toast"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../../components/ui/form"
 import { Button } from "../../components/ui/button"
 import { useForm } from "react-hook-form"
+import { useState } from "react"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/app/firebaseConfig"
+import { GridLoader } from "react-spinners"
 
 
 const FormSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-   email: z
-    .string()
-    .min(1, { message: "This field has to be filled." })
-    .email("This is not a valid email.")
-    .refine((e) => e === "abcd@fg.com", "This email is not in our database"),
+  emailAddress: z.string().email(),
+  //  email: z
+  //   .string()
+  //   .min(1, { message: "This field has to be filled." })
+  //   .email("This is not a valid email.")
+  //   .refine((e) => e === "abcd@fg.com", "This email is not in our database"),
    password: z.string().min(4),
 })
 
 export function LoginForm() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
+  const router = useRouter()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("")
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+          emailAddress: "",
+          password:"",
+        },
+      })
+        //  Define a submit handler.
+  const handleSubmit = async(values: z.infer<typeof FormSchema>) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+        if (loading) {
+          return <GridLoader color="#e21ba6" /> ; // Show spinner while loading
+        }
+      console.log("Sign in successful")
+      router.push("/dashboard"); // Redirect to home or dashboard page
+    } catch (error: any) {
+      setError(getErrorMessage(error.code))
+      console.error("Error signing in:",error.code);
+    }  
+    finally {
+      setLoading(false);
+    }
+    // console.log(values) 
+    
   }
+   // Function to map Firebase error codes to user-friendly messages
+  const getErrorMessage = (code: any) => {
+    switch (code) {
+      case 'auth/invalid-credential':
+        return 'Incorrect user name or password. Please try again.';     
+      default:
+        return 'An error occurred. Please try again.';
+    }
+  };
 
   return (
     <main className="flex-col mx-auto mt-20">
       <h1 className="text-3xl text-center">Login Form</h1>
       <div className="flex justify-center items-center max-w-lg mx-auto">
           <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 p-4 rounded-md space-y-4 bg-slate-100">
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="w-2/3 p-4 rounded-md space-y-4 bg-slate-100">
                                 
             <FormField
             control={form.control}
-            name="email"
+            name="emailAddress"
             render={({ field }) => (
               <FormItem>
                 <FormControl>
-                  <Input placeholder="Email" {...field} />
+                  <Input placeholder="Email"
+                    value={field.value}
+                      onChange={(e) => {
+                        // call field.onchange handler
+                        field.onChange(e);
+                        setEmail(e.target.value)
+                      }}
+                    />
                 </FormControl>              
                 <FormMessage />
               </FormItem>
@@ -68,7 +103,14 @@ export function LoginForm() {
             render={({ field }) => (
               <FormItem>
                 <FormControl>
-                  <Input placeholder="Password" {...field} />
+                  <Input placeholder="Password"
+                    value={field.value}
+                    onChange={(e) => {
+                      // call field.onchange handler
+                      field.onChange(e);
+                      setPassword(e.target.value)
+                    }}
+                  />
                 </FormControl>              
                 <FormMessage />
               </FormItem>
